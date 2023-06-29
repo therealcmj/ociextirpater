d4671e26f0723c25e37f3d8a4f09742f75169052
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2023-06-28 20:07:56 -0400

    Actually implement Log Analytics cleanup

--- ociextirpater/ociclients/loganalytics.py
+++ ociextirpater/ociclients/loganalytics.py
@@ -1,17 +1,39 @@
 import oci
 from ociextirpater.OCIClient import OCIClient
 
+import logging
+
 class loganalytics( OCIClient ):
     service_name = "Log Analytics"
     clientClass = oci.log_analytics.LogAnalyticsClient
 
+    namespace = None
+    def __init__(self,config):
+        super().__init__(config)
+
+        # we need the namespace
+        self.namespace = self.clients[ config.home_region ].list_namespaces(config.ociconfig["tenancy"]).data.items[0].namespace_name
+        logging.debug("Logging Analytics namespace is {}".format(self.namespace))
+
     objects = [
+        # # this is a very special one - storage
         # {
-        #     "function_list"      : "list_log_analytics_entities",
-        #     "function_delete"    : "delete_log_analytics_entities",
-        #     "name_singular"      : "Log Analytics Entity",
-        #     "name_plural"        : "Log Analytics Entities",
+        #     "name_singular"      : "Storage",
+        #     "name_plural"        : "Storage",
+        #
+        #     # # "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
+        #     # "function_list"      : "list_xxx",
+        #     # "function_delete"    : "delete_xxx",
         # },
+
+        {
+            "formatter"          : lambda obj: "Log Analytics Entity with OCID {} / name '{}' is in state {}".format( obj.id, obj.name, obj.lifecycle_state ),
+
+            "function_list"      : "list_log_analytics_entities",
+            # "function_delete"    : "delete_log_analytics_entity",
+            "name_singular"      : "Log Analytics Entity",
+            "name_plural"        : "Log Analytics Entities",
+        },
 
         # {
         #     "function_list"      : "list_namespaces",
@@ -22,7 +44,7 @@ class loganalytics( OCIClient ):
 
         {
             "function_list"      : "list_log_analytics_log_groups",
-            "function_delete"    : "delete_log_analytics_log_group",
+            # "function_delete"    : "delete_log_analytics_log_group",
             "name_singular"      : "Log Analytics Log Group",
             "name_plural"        : "Log Analytics Log Groups",
         },
@@ -36,7 +58,26 @@ class loganalytics( OCIClient ):
         # },
     ]
 
+    # I override the findAllInCompartment from OCIClient.py because Log Analytics has an extra param
+    def findAllInCompartment(self, region, o, this_compartment, **kwargs):
+        logging.debug("Calling {}".format( o["function_list"]))
+        os = oci.pagination.list_call_get_all_results(getattr((self.clients[region]), o["function_list"]),
+                                                      self.namespace,
+                                                      this_compartment,
+                                                      **kwargs).data
+        return os
 
-    # def findAllInCompartment(self, region, o, this_compartment, **kwargs):
-    #     # self.clients[region].list_log_analytics_log_groups()
-    #     pass
+    def delete_object(self, object, region, found_object):
+        if object["name_singular"] == "Log Analytics Entity":
+            return (self.clients[region]).delete_log_analytics_entity(
+                self.namespace,
+                found_object.id
+            )
+
+        elif object["name_singular"] == "Log Analytics Log Group":
+            return (self.clients[region]).delete_log_analytics_log_group(
+                self.namespace,
+                found_object.id
+            )
+
+        raise NotImplementedError
