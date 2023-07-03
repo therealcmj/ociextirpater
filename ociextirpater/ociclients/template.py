97ce906dcb1327909f97e0c934e55b966e28dbc1
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2023-07-03 11:05:30 -0400

    README and template.py
    I should probably tell people what it is, what it does, and provide the template I start with when adding a new service.
    
    So I did.

---
+++ ociextirpater/ociclients/template.py
@@ -0,0 +1,18 @@
+import oci
+from ociextirpater.OCIClient import OCIClient
+
+class xxx( OCIClient ):
+    service_name = "XXX"
+    clientClass = oci.xxx.YYY
+
+    objects = [
+        # {
+        #     # "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
+        #     "function_list"      : "list_xxx",
+        #     "kwargs_list"        : {
+        #                            },
+        #     "function_delete"    : "delete_xxx",
+        #     "name_singular"      : "XXX",
+        #     "name_plural"        : "XXXXs",
+        # },
+    ]
