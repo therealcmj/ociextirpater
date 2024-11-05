2231f27346d52d86da0b304ca763ec8933aecfd0
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2024-11-05 16:47:05 -0500

    Add additional comments in compute.py
    Comments are good. Add them so people know why I did what I did.

--- ociextirpater/ociclients/compute.py
+++ ociextirpater/ociclients/compute.py
@@ -17,14 +17,18 @@ class compute( OCIClient ):
             "function_list"      : "list_instances",
             "function_delete"    : "terminate_instance",
             "kwargs_delete"      : {
-                "preserve_data_volumes_created_at_launch": False
-            }
+                                       "preserve_data_volumes_created_at_launch": False
+                                   }
         },
 
         {
             "name_singular"      : "Compute Image",
             "name_plural"        : "Compute Images",
             "function_list"      : "list_images",
+                                    # listing images returns all the images in the specified compartment PLUS those
+                                    # provided by Oracle. You can easily tell the difference by checking whether
+                                    # it has compartment_id = None or a string.
+                                    # This lambda function causes delete to skip those provided by Oracle
             "check2delete"       : lambda image: hasattr( image,"compartment_id") and image.compartment_id != None,
             "function_delete"    : "delete_image",
         },
