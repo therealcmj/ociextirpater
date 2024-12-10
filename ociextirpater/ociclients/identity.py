1d856f3ae98cd5d786b6c17052714e96aad47006
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2024-12-10 09:03:51 -0500

    Removing incomplete implementation of compartments from identity class
    Compartments has been moved to its own separate class in a previous commit.

--- ociextirpater/ociclients/identity.py
+++ ociextirpater/ociclients/identity.py
@@ -9,7 +9,6 @@ class identity( OCIClient ):
     isRegional = False
 
     objects = [
-
         {
             "name_singular"      : "Policy",
             "name_plural"        : "Policies",
@@ -18,27 +17,5 @@ class identity( OCIClient ):
             "function_delete"    : "delete_policy",
             "formatter"          : lambda policy: "Policy with OCID {} / name '{}' is in state {}".format(policy.id,policy.name,policy.lifecycle_state),
         },
-
-        {
-            "function_list"      : "list_compartments",
-            # "function_delete"    : "delete_compartment",
-            "name_singular"      : "Compartment",
-            "name_plural"        : "Compartments",
-            "formatter"          : lambda compartment: "Compartment with OCID {} / name '{}' is in state {}".format(compartment.id,
-                                                                                                                    compartment.name,
-                                                                                                                    compartment.lifecycle_state),
-        },
-
-        # {
-        #     # "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
-        #     "function_list"      : "list_xxx",
-        #     "kwargs_list"        : {
-        #                            },
-        #     "function_delete"    : "delete_xxx",
-        #     "name_singular"      : "XXX",
-        #     "name_plural"        : "XXXXs",
-        # },
     ]
 
-    def predelete(self,object,region,found_object):
-        self.compositeClients[region]
