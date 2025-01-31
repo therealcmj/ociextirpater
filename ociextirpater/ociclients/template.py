160331e1cc2efc353703b3da1528092d923e5bf0
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-01-31 14:54:42 -0500

    Update template.py to show more examples

--- ociextirpater/ociclients/template.py
+++ ociextirpater/ociclients/template.py
@@ -1,3 +1,4 @@
+import logging
 import oci
 from ociextirpater.OCIClient import OCIClient
 
@@ -6,6 +7,13 @@ class xxx( OCIClient ):
     clientClass = oci.xxx.YYY
 
     objects = [
+        # {
+        #     "name_singular"      : "XXX",
+        #     "name_plural"        : "XXXXs",
+        #     "function_list"      : "list_xxx",
+        #     "function_delete"    : "delete_xxx",
+        # },
+
         # {
         #     "name_singular"      : "XXX",
         #     "name_plural"        : "XXXXs",
@@ -18,3 +26,24 @@ class xxx( OCIClient ):
         #     "function_delete"    : "delete_xxx",
         # },
     ]
+
+
+
+    # def predelete(self,object,region,found_object):
+    #     if object["name_plural"] == "XXX":
+    #         f = getattr((self.clients[region]), "update_XXX")
+    #         logging.info("info message here")
+    #         f( found_object.id, { "something": True } )
+    #         return
+    #
+    #     raise NotImplementedError
+
+    # def delete_object(self, object, region, found_object):
+    #     if object["name_plural"] == "DNS Resolver Endpoints":
+    #         # DNS Resolver Endpoints
+    #         f = getattr((self.clients[region]), "delete_resolver_endpoint")
+    #         logging.info( "Deleting {}".format(object["name_singular"]) )
+    #         f( found_object["endpoint_id"], found_object["name"] )
+    #         return
+    #
+    #     raise NotImplementedError
