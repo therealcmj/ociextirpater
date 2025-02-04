c20b2ea09ccfeeb515d011765326fb4f8618b1c1
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-02-04 12:29:56 -0500

    Improve Functions cleanup
    Functions Apps would (previously) only be deleted if there were no Functions in the App. This fixes that limitation.

--- ociextirpater/ociclients/functions.py
+++ ociextirpater/ociclients/functions.py
@@ -1,3 +1,4 @@
+import logging
 import oci
 from ociextirpater.OCIClient import OCIClient
 
@@ -7,9 +8,28 @@ class functions( OCIClient ):
 
     objects = [
         {
-            "function_list"    : "list_applications",
-            "function_delete"  : "delete_application",
             "name_singular"    : "Functions Application",
             "name_plural"      : "Functions Applications",
+            "function_list"    : "list_applications",
+            "function_delete"  : "delete_application",
         }
     ]
+
+
+    def predelete(self,object,region,found_object):
+        if object["name_plural"] == "Functions Applications":
+            # you cannot delete a Functions Application until you delete all the Functions in it
+            fList   = getattr((self.clients[region]), "list_functions")
+            fDelete = getattr((self.clients[region]), "delete_function")
+
+            logging.info("Getting all Functions in Application {}".format( found_object.id ))
+            # f( found_object.id, { "something": True } )
+
+            fns = oci.pagination.list_call_get_all_results(fList,found_object.id).data
+            logging.debug("Found {} Functions in Function App {}".format( len ( fns ), found_object.id))
+            for fn in fns:
+                logging.debug("Deleting Function {}".format( fn.id) )
+                fDelete( fn.id )
+            return
+
+        raise NotImplementedError
