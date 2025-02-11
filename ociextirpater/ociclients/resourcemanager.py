c8c2a10bca088f6bfc38159968ce78943dd1e4a4
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-02-11 18:05:29 -0500

    Private Endpoint support for Resource Manager
    and also fix "delete" for jobs - there's no such thing as delete_job, but there is cancel_job. Oops

--- ociextirpater/ociclients/resourcemanager.py
+++ ociextirpater/ociclients/resourcemanager.py
@@ -17,8 +17,15 @@ class resourcemanager( OCIClient ):
             "name_singular"      : "Job",
             "name_plural"        : "Jobs",
 
-            "function_delete"    : "delete_job",
+            "function_delete"    : "cancel_job",
         },
+
+        {
+            "name_singular"      : "Private Endpoint",
+            "name_plural"        : "Private Endpoints",
+
+            "function_delete"    : "delete_private_endpoint",
+        }
     ]
 
     def list_objects(self, o, region, this_compartment, **kwargs):
@@ -30,6 +37,11 @@ class resourcemanager( OCIClient ):
         if o["name_plural"] == "Jobs":
             return oci.pagination.list_call_get_all_results(
                 getattr((self.clients[region]), "list_jobs"),
+                **{"compartment_id":this_compartment}).data
+
+        if o["name_plural"] == "Private Endpoints":
+            return oci.pagination.list_call_get_all_results(
+                getattr((self.clients[region]), "list_private_endpoints"),
                 **{"compartment_id":this_compartment}).data
 
         raise NotImplementedError
