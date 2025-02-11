740a592eb7dd14b726ce680618356ca671341b15
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-02-11 18:00:52 -0500

    Add versioned object support to Object Store client code

--- ociextirpater/ociclients/objectstore.py
+++ ociextirpater/ociclients/objectstore.py
@@ -1,4 +1,5 @@
 import logging
+import concurrent
 
 import oci
 from ociextirpater.OCIClient import OCIClient
@@ -11,9 +12,17 @@ class objectstore( OCIClient ):
 
     objects = [
         {
+            "name_singular"    : "Private Endpoint",
+            "name_plural"      : "Private Endpoints",
+            "function_list"    : "list_private_endpoints",
+            "function_delete"  : "delete_private_endpoint",
+        },
+
+        {
             "name_singular"    : "Object Store bucket",
             "name_plural"      : "Object Store buckets",
             "formatter"        : lambda bucket: "Bucket with name '{}'".format(bucket.name),
+            "function_list"    : "list_buckets",
             # Why *did* I have this?
             # Buckets don't have a lifecycle state. And originally I didn't have hasattr() to check
             # for that attribute. So this lambda function was here to avoid an exception being thrown.
@@ -50,7 +59,14 @@ class objectstore( OCIClient ):
                                         "function_delete": "delete_object",
                                         "name_singular"  : "Object",
                                         "name_plural"    : "Objects",
-                                    }
+                                    },
+                                    {
+                                        "function_list"  : "list_object_versions",
+                                        "function_delete": "delete_object",
+                                        "name_singular"  : "Object Version",
+                                        "name_plural"    : "Object Versions",
+                                    },
+
             ]
 
         }
@@ -65,13 +81,27 @@ class objectstore( OCIClient ):
 
 
     def list_objects(self, o, region, this_compartment, **kwargs):
-        if o["name_plural"] == "Object Store buckets":
-            return oci.pagination.list_call_get_all_results(    getattr((self.clients[region]), "list_buckets"),
-                                                                self.namespace,
-                                                                this_compartment,
-                                                                **kwargs).data
-        else:
-            raise NotImplementedError
+        if o["name_plural"] == "Object Store buckets" or o["name_plural"] == "Private Endpoints":
+            logging.debug("Calling oci.pagination.list_call_get_all_results( {}()...".format(o["function_list"]))
+
+            return oci.pagination.list_call_get_all_results(getattr((self.clients[region]), o["function_list"]),
+                                                            self.namespace,
+                                                            this_compartment,
+                                                            **kwargs).data
+
+        # # TODO: combine these
+        # if o["name_plural"] == "Object Store buckets":
+        #     return oci.pagination.list_call_get_all_results(    getattr((self.clients[region]), "list_buckets"),
+        #                                                         self.namespace,
+        #                                                         this_compartment,
+        #                                                         **kwargs).data
+        # if o["name_plural"] == "Private Endpoints":
+        #     return oci.pagination.list_call_get_all_results(    getattr((self.clients[region]), "list_private_endpoints"),
+        #                                                         self.namespace,
+        #                                                         this_compartment,
+        #                                                         **kwargs).data
+
+        raise NotImplementedError
 
     def delete_object(self, oci_object, region, object):
         f = None
@@ -94,10 +124,11 @@ class objectstore( OCIClient ):
                     # then we need to do something special
                     logging.info("Deleting objects in bucket")
 
-                    import concurrent
                     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
                         for obj in xs.objects:
-                            future = executor.submit(df, self.namespace, object.name, obj.name )
+                            logging.debug("Deleting {}".format(obj.name))
+
+                            future = executor.submit(df, self.namespace, object.name, obj.name)
 
                 else:
                     for x in xs:
@@ -109,6 +140,17 @@ class objectstore( OCIClient ):
                                 df(self.namespace, object.name, x.id )
                             except:
                                 logging.debug("delete failed, but continuing...")
+                        elif child["name_plural"] == "Object Versions":
+                            logging.info("Deleting object versions...ß")
+                            # for x in xs:
+                            #     logging.debug("Deleting {} version {}".format( x.name, x.version_id ))
+                            #     df( object.namespace, object.name, x.name, **{"version_id": x.version_id} )
+                            with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+                                for x in xs:
+                                    logging.debug("Deleting {} version {}".format( x.name, x.version_id ))
+                                    future = executor.submit(df, object.namespace, object.name, x.name, **{"version_id": x.version_id} )
+
+                            logging.debug("Done deleting object versions")
                         else:
                             df( self.namespace, object.name, x.id )
 
@@ -117,6 +159,10 @@ class objectstore( OCIClient ):
             f( self.namespace, object.name )
 
             f = getattr((self.clients[region]), "delete_bucket")
+            logging.debug("calling delete method")
+            f(self.namespace, object.name)
+        elif oci_object["name_singular"] == "Private Endpoint":
+            f = getattr((self.clients[region]), "delete_private_endpoint")
             logging.debug("calling delete method")
             f(self.namespace, object.name)
         else:
