607706b7a4283b55f7fc5f071b12635a5acb33fa
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-04-07 17:27:20 -0400

    adding delete protection for mySQL database systems

--- ociextirpater/ociclients/mysql.py
+++ ociextirpater/ociclients/mysql.py
@@ -1,4 +1,7 @@
+import logging
 import oci
+from oci.mysql.models import DeletionPolicyDetails
+
 from ociextirpater.OCIClient import OCIClient
 
 class mysql( OCIClient ):
@@ -11,7 +14,28 @@ class mysql( OCIClient ):
             "name_plural"        : "MySQL DB Systems",
 
             "function_list"      : "list_db_systems",
-        #     "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
             "function_delete"    : "delete_db_system",
         },
     ]
+
+    def predelete(self,object,region,found_object):
+        if object["name_plural"] == "MySQL DB Systems":
+            if not found_object.deletion_policy.is_delete_protected:
+                logging.debug("{} is not delete protected".format(object["name_singular"]))
+                return
+
+
+            f = getattr((self.clients[region]), "update_db_system")
+            logging.info("Updating DB system to remove delete protection")
+            # dp = found_object.deletion_policy
+            # dp.is_delete_protected = False
+            dp = {
+                    "automaticBackupRetention": DeletionPolicyDetails.AUTOMATIC_BACKUP_RETENTION_DELETE,
+                    "finalBackup": DeletionPolicyDetails.FINAL_BACKUP_SKIP_FINAL_BACKUP,
+                    "isDeleteProtected": False
+            }
+            found_object.deletion_policy.is_delete_protected = False
+            f( found_object.id, { "deletionPolicy" : dp } )
+            return
+
+        raise NotImplementedError
