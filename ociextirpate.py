697f0dc86a559b034387db83295772db237258a9
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2025-07-07 19:35:59 -0400

    Patch for "HttpConnectionPool is full, discarding connection" log line
    Added a bit of a hack to the HTTP request pooling code in Python. The message is harmless but performance is slightly lower than it could be.
    
    This code overdoes it but the error is now gone.

--- ociextirpate.py
+++ ociextirpate.py
@@ -5,6 +5,33 @@ import importlib
 import logging
 
 __requires__ = 'oci==2.78.0'
+__requires__ = 'oci==2.144.0'
+
+
+### NASTY
+### https://stackoverflow.com/questions/18466079/change-the-connection-pool-size-for-pythons-requests-module-when-in-threading
+###
+### sorry, not sorry
+def patch_http_connection_pool(**constructor_kwargs):
+    """
+    This allows to override the default parameters of the
+    HTTPConnectionPool constructor.
+    For example, to increase the poolsize to fix problems
+    with "HttpConnectionPool is full, discarding connection"
+    call this function with maxsize=16 (or whatever size
+    you want to give to the connection pool)
+    """
+
+    from oci._vendor.urllib3 import connectionpool, poolmanager
+
+    class MyHTTPConnectionPool(connectionpool.HTTPConnectionPool):
+        def __init__(self, *args,**kwargs):
+            kwargs.update(constructor_kwargs)
+            super(MyHTTPConnectionPool, self).__init__(*args,**kwargs)
+    poolmanager.pool_classes_by_scheme['http'] = MyHTTPConnectionPool
+    # poolmanager.pool_classes_by_scheme['https'] = MyHTTPConnectionPool
+
+patch_http_connection_pool(maxsize=1000)
 
 def extirpate():
 
