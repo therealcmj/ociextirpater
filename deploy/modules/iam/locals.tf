60a36c0b76ef27ecd58d362605f1c65abecc8912
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2026-02-25 09:42:25 -0500

    Terraform fix on policies

--- deploy/modules/iam/locals.tf
+++ deploy/modules/iam/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  scheduler_statements = [for id in var.scheduler_id : "Allow any-user to manage instance in compartment id ${var.extirpate_compartment} where all {request.principal.type='resourceschedule',request.principal.id='${id}'}"]
+  scheduler_statements = [for id in var.scheduler_id : "Allow any-user to manage instance in compartment id ${var.resources_compartment} where all {request.principal.type='resourceschedule',request.principal.id='${id}'}"]
   dynamic_group_statements = ["Allow dynamic-group ${oci_identity_dynamic_group.this.name} to manage all-resources in compartment id ${var.extirpate_compartment}"]
 }
