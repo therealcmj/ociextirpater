fd519c9102205f32eff84fcc1e10ac1f79f0e057
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2026-05-07 21:31:25 -0400

    Enhancements to WAF cleanup

--- ociextirpater/ociclients/waf.py
+++ ociextirpater/ociclients/waf.py
@@ -4,17 +4,37 @@ from ociextirpater.OCIClient import OCIClient
 class waf( OCIClient ):
     service_name = "Web App Firewall"
     clientClass = oci.waf.WafClient
+    compositeClientClass = oci.waf.WafClientCompositeOperations
+
+    _kwargs_delete_wait = {
+                        "wait_for_states": [
+                            oci.waf.models.WorkRequest.STATUS_SUCCEEDED,
+                            oci.waf.models.WorkRequest.STATUS_FAILED,
+                            ]
+                    }
 
     objects = [
         {
             "name_singular"      : "Web App Firewall",
             "name_plural"        : "Web App Firewalls",
+            "function_list"      : "list_web_app_firewalls",
+            # in order to delete the policies you need to wait for all firewalls to be deleted. The next two lines allows for that.
+            "c_function_delete"  : "delete_web_app_firewall_and_wait_for_state",
+            "kwargs_delete"      :  _kwargs_delete_wait,
+        },
 
+        {
+            "name_singular"      : "Web App Firewall Policy",
+            "name_plural"        : "Web App Firewall Policies",
             "function_list"      : "list_web_app_firewall_policies",
-            # "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
             "function_delete"    : "delete_web_app_firewall_policy",
         },
+
+        {
+            "name_singular"      : "Web App Firewall Network address list",
+            "name_plural"        : "Web App Firewall Network address lists",
+            "function_list"      : "list_network_address_lists",
+            "function_delete"    : "delete_network_address_list",
+        },
+
     ]
-
-
-#list_network_address_lists
