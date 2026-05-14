79e59d894fcc9dfd0d5a5d67296edda1fc2060e2
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2026-05-13 21:28:03 -0400

    Added stopping Data Pool as a pre-delete action.

--- ociextirpater/ociclients/dataflow.py
+++ ociextirpater/ociclients/dataflow.py
@@ -5,14 +5,16 @@ from ociextirpater.OCIClient import OCIClient
 class dataflow( OCIClient ):
     service_name = "Data Flow"
     clientClass = oci.data_flow.DataFlowClient
+    compositeClientClass = oci.data_flow.DataFlowClientCompositeOperations
 
     objects = [
-        # {
-        #     "name_singular"      : "Run",
-        #     "name_plural"        : "Runs",
-        #     "function_list"      : "list_runs",
-        #     "function_delete"    : "delete_run",
-        # },
+        {
+            "name_singular"      : "Run",
+            "name_plural"        : "Runs",
+            "function_list"      : "list_runs",
+            "check2delete"       : lambda run: run.lifecycle_state != "FAILED",
+            "function_delete"    : "delete_run",
+        },
 
         {
             "name_singular"      : "Application",
@@ -36,3 +38,35 @@ class dataflow( OCIClient ):
         },
 
     ]
+
+
+    def predelete(self,object,region,found_object):
+        if object["name_plural"] == "Pools":
+            # anything that is not stopped needs to be stopped before it can be deleted.
+            logging.info("Pool is currently in lifecycle state {}".format(found_object.lifecycle_state))
+            if found_object.lifecycle_state != found_object.LIFECYCLE_STATE_STOPPED:
+                logging.info("Stopping")
+                
+                # f = getattr((self.compositeClients[region]), "stop_pool_and_wait_for_state")
+                # f( 
+                #     found_object.id,
+                #     wait_for_states=[ 
+                #         oci.data_flow.models.WorkRequest.STATUS_SUCCEEDED
+                #         ]
+                # )
+
+                f = getattr((self.clients[region]), "stop_pool")
+                f(found_object.id)
+
+                logging.debug("Waiting for data flow pool to stop")
+                oci.waiter.wait_until(
+                    self.clients[region],
+                    self.clients[region].get_pool(found_object.id),
+                    evaluate_response=lambda r: r.data.lifecycle_state == oci.data_flow.models.Pool.LIFECYCLE_STATE_STOPPED,
+                    max_wait_seconds=1200
+                )
+
+                logging.info("Pool stopped and now ready for deletion")
+                return
+                
+        raise NotImplementedError
