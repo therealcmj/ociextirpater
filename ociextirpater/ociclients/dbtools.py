ccf7bc3340b46e857157bafc80b78e977f5c4a75
Chris Johnson <chris@three12.com>
2026-09-21 14:38:42 -0400

    DBTools updates
    * add MCP server related
    * cleanup and reorganize

--- ociextirpater/ociclients/dbtools.py
+++ ociextirpater/ociclients/dbtools.py
@@ -1,39 +1,51 @@
 import oci
+
 from ociextirpater.OCIClient import OCIClient
 
-class dbtools( OCIClient ):
+
+class dbtools(OCIClient):
     service_name = "Database Tools"
     clientClass = oci.database_tools.DatabaseToolsClient
+    compositeClientClass = oci.database_tools.DatabaseToolsClientCompositeOperations 
+
+    _kwargs_delete_wait = {
+                        "wait_for_states": [
+                            oci.database_tools.models.WorkRequest.STATUS_SUCCEEDED,
+                            oci.database_tools.models.WorkRequest.STATUS_FAILED,
+                            ]
+                    }
 
     objects = [
-
-        # list_database_tools_connections(…)
         {
-            "function_list"      : "list_database_tools_connections",
-            # "kwargs_list"        : {
-            #                             # "lifecycle_state": ["ACTIVE","FAILED"]
-            #                        },
-            "function_delete"    : "delete_database_tools_connection",
+            "name_singular"      : "Database Tools MCP Toolset",
+            "name_plural"        : "Database Tools MCP Toolsets",
+            "function_list"      : "list_database_tools_mcp_toolsets",
+            # "function_delete"    : "delete_database_tools_mcp_toolset",
+            "c_function_delete"  : "delete_database_tools_mcp_toolset_and_wait_for_state",
+            "kwargs_delete"      :  _kwargs_delete_wait
+        },
+
+        {
+            "name_singular"      : "Database Tools MCP Server",
+            "name_plural"        : "Database Tools MCP Servers",
+            "function_list"      : "list_database_tools_mcp_servers",
+            # "function_delete"    : "delete_database_tools_mcp_server",
+            # "function_delete"    : "cascading_delete_database_tools_mcp_server"
+            "c_function_delete"  : "delete_database_tools_mcp_server_and_wait_for_state",
+            "kwargs_delete"      :  _kwargs_delete_wait
+        },
+
+        {
             "name_singular"      : "Database Tools Connection",
             "name_plural"        : "Database Tools Connections",
+            "function_list"      : "list_database_tools_connections",
+            "function_delete"    : "delete_database_tools_connection",
         },
 
-        # list_database_tools_private_endpoints(…)
         {
-            "function_list"      : "list_database_tools_private_endpoints",
-            "function_delete"    : "delete_database_tools_private_endpoint",
             "name_singular"      : "Database Tools Private Endpoint",
             "name_plural"        : "Database Tools Private Endpoints",
+            "function_list"      : "list_database_tools_private_endpoints",
+            "function_delete"    : "delete_database_tools_private_endpoint",
         },
-
-        # {
-        #     # "formatter"          : lambda instance: "XXX instance with OCID {} / name '{}' is in state {}".format( instance.id, instance.name, instance.lifecycle_state ),
-        #     "function_list"      : "list_xxx",
-        #     "kwargs_list"        : {
-        #                            },
-        #     "function_delete"    : "delete_xxx",
-        #     "name_singular"      : "XXX",
-        #     "name_plural"        : "XXXXs",
-        # },
-
     ]
